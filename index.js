9f6fc18607ba21444a4f5c0d9ad45306f3b52059
andrej <andrej.bartko@gmail.com>
2015-07-26 22:46:13 +0100

    methods

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ var Parser = function(){
 
 }
 
+/**
+ * returns the header objects
+ *
+ * @returns {Object} Key value pairs containing the header info
+ */
+Parser.prototype.getHeader = function(){
+  var header = {
+    type: '',
+    size: 0,
+    data:{
+      start: null,
+      end: null,
+      size: 0,
+      endianess: null
+    },
+    analysis: {
+      start: null,
+      end: null
+    }
+  };
+  return header;
+};
+
+/**
+ * return parameters object
+ *
+ * @returns {Object} key value pairs param name param value
+ */
+Parser.prototype.getParams(){
+  var params = {}
+
+  return params;
+};
+
 Parser.init = function(filePath){
   var file;
   try {
@@ -13,8 +47,7 @@ Parser.init = function(filePath){
     throw new Error('BAAAAAD');
   }
 
-  return {
-  };
+  return new Parser(file);
 }
 
 
