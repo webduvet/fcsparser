b47e569c18afae5b991f0f208bfd93ff2f28132c
andrej <andrej.bartko@gmail.com>
2015-07-27 23:09:40 +0100

    progress

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var fs = require('fs');
 
 var Parser = function(){
-
 }
+
+// fcs3 starts at 0 versiom takes 10 bytes as text, then
+// every other number takes 8 bytes
+// header has length 58 bytes
+var offsets = {
+  v2: [],
+  v3: {
+    version: [0],
+    text: [10, 18], // start end, 8 bytes each position
+    data: [26, 34],
+    analysis: [42, 50]
+  }
+}
+
+var FcsStream = function(path) {
+  var buffer = new Buffer(58);
+  console.log(buffer);
+  fs.readSync(path, buffer, 0, 58, 0);
+  console.log(buffer);
+}
+
+var fcs = new FcsStream('./test/mockdata/test1.fcs');
 
 Parser.parseHeader = function(file) {
   var header = {
