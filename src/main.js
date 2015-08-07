c80308b3b8c18db7c9429432a3a77855a04df469
andrej <andrej.bartko@gmail.com>
2015-08-07 06:22:14 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,11 +1,39 @@
 var 
-	hp = require('./headerparser.js'),
-	tp = require('./textparser.js'),
-	Praser
+	Reader
 ;
 
-Parser = function(fd) {
+/**
+ * @constructor
+ * @param {Object} Text segment
+ */
+Reader = function(text, data) {
+  this._params = {
+  }.
+
+  this._text = text;
+  this._data = data;
 	
 }
 
-module.exports = Parser;
+/**
+ * reads to position to plot the graph
+ */
+Reader.prototype.readPosition = function(p1, p2, pos) {
+  return {
+  this._text.getParam(p1)
+};
+
+/**
+ * loads the specfied parameter into memory
+ */
+Reader.prototype.loadParameter = function(par) {
+}
+
+/**
+ * frees memory alocated by parameter and make it
+ * available for gc
+ */
+Reader.prototype.throwParameter = function(par) {
+}
+
+module.exports = Reader;
